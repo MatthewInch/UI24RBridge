1e3d0db320cd5eb81f969527d4d5d30bf3c4f471
Szőke, Mátyás <inch@szuteren.hu>
2019-12-22 22:16:48 +0100

    Create first classes

---
+++ UI24RController/UI24RBridge.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.WebSockets;
+
+namespace UI24RController
+{
+    public class UI24RBridge
+    {
+        /// <summary>
+        /// Represent the bridge between the UI24R and a DAW controller
+        /// </summary>
+        /// <param name="address"></param>
+        public UI24RBridge(string address, IMIDIController midiController)
+        {
+
+        }
+    }
+}
