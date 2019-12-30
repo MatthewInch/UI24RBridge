f116cdb2ee3859df408ba04766134cb523d8c25c
Szőke, Mátyás <inch@szuteren.hu>
2019-12-31 00:58:01 +0100

    Add protocol settings to the test project

---
+++ UI24RController/MIDIController/MIDIControllerBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UI24RController.MIDIController
+{
+    public abstract class MIDIControllerBase : IMIDIController
+    {
+        public static IMIDIController GetMidiController(string protocol)
+        {
+            switch (protocol)
+            {
+                case "HUI":
+                    return new MackieHUI();
+                default:
+                    return new BehringerUniversalMIDI();
+            }
+        }
+
+        public event EventHandler<MessageEventArgs> MessageReceived;
+        public event EventHandler<FaderEventArgs> FaderEvent;
+
+        public bool ConnectInputDevice(string deviceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ConnectOutputDevice(string deviceName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetInputDeviceNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] GetOutputDeviceNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetFader(int channelNumber, double faderValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
